95a759bd61b6b16a09bd256f94129cff7fe23881
Pedro Conde <pedroconde@mail.usf.edu>
2018-04-11 18:17:56 -0400

    Add files via upload

---
+++ Euler10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Euler10
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{ 
+			int tm = 20000000;
+			int t = 1;
+			int sum = 0;
+
+			while (t < tm) {
+				if (t % 2 != 0 | t % 3 != 0) {
+					sum += t;
+					t++;
+				} else {
+					t++;
+				}
+			}
+
+			Console.WriteLine ("The sum of the prime numbers below two million is: " + sum);
+		} 
+	}
+}
